2236b8f822be6f8bb3940c53226421ccdebfc2b8
estudiante <karolaiturry@gmail.com>
2025-05-21 07:51:43 -0500

    Karol no hizo nada en todo el dia

--- app/src/main/java/com/grupo2/ibudget/MainActivity.kt
+++ app/src/main/java/com/grupo2/ibudget/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            //KAROL DAÑO ESTO!!
             IbudgetTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
