144d43c2d24b40ddd33a48c485fb77c59ab1b02c
estudiante <karolaiturry@gmail.com>
2025-06-20 10:53:37 -0500

    "se realizaron los botones de registro de usuario y el inicio de sesion , se le implemento los incons para la contraseña "
    "se aliniaron los botones , se le agregaron color a los botones "
    
    "se creo una nueva pantalla"

---
+++ app/src/main/java/com/grupo2/ibudget/ui/screens/RachaScreen .kt
@@ -0,0 +1,13 @@
+package com.grupo2.ibudget.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.grupo2.ibudget.ui.theme.IbudgetTheme
+
+@Preview(showBackground = true)
+@Composable
+fun RachaScreenPreview() {
+    IbudgetTheme {
+        Login()
+    }
+}
