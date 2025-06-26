f6063b97f3d51464383bb9ed07444e74bc08156c
estudiante <karolaiturry@gmail.com>
2025-06-26 08:56:58 -0500

    "se realizaron los botones de registro de usuario y el inicio de sesion , se le implemento los incons para la contraseña "
    "se aliniaron los botones , se le agregaron color a los botones "
    
    "se creo una nueva pantalla , se realizo una columna y un row"

--- app/src/main/java/com/grupo2/ibudget/ui/screens/RachaScreen .kt
+++ app/src/main/java/com/grupo2/ibudget/ui/screens/RachaScreen .kt
@@ -1,16 +1,23 @@
 package com.grupo2.ibudget.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.grupo2.ibudget.R
 import com.grupo2.ibudget.ui.theme.IbudgetTheme
 import com.grupo2.ibudget.ui.theme.Pink80
 
@@ -20,14 +27,16 @@ fun RachaScreen() {
     Column (
         modifier = Modifier
             .padding(8.dp)
+            .background(color = Color.LightGray)
             .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Row(modifier = Modifier.
         fillMaxWidth()
             .background(color = Pink80)
-            .padding(25.dp)
+            .padding(40.dp)
         ) {
+
 
         }
     }
