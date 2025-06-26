1b8d196bf24357480cbaef9e801ee4ae1738002d
estudiante <karolaiturry@gmail.com>
2025-06-26 10:59:38 -0500

    "se cambio el color de la columna y se agrego un icons para la racha"

--- app/src/main/java/com/grupo2/ibudget/ui/screens/RachaScreen .kt
+++ app/src/main/java/com/grupo2/ibudget/ui/screens/RachaScreen .kt
@@ -1,48 +1,62 @@
 package com.grupo2.ibudget.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.grupo2.ibudget.R
 import com.grupo2.ibudget.ui.theme.IbudgetTheme
 import com.grupo2.ibudget.ui.theme.Pink80
 
 @Composable
 fun RachaScreen() {
-
-    Column (
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.fire_anim))
+    val progress by animateLottieCompositionAsState(composition)
+    Column(
         modifier = Modifier
             .padding(8.dp)
-            .background(color = Color.LightGray)
+            .background(color = Pink80)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Row(modifier = Modifier.
-        fillMaxWidth()
-            .background(color = Pink80)
-            .padding(40.dp)
-        ) {
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+
+        LottieAnimation(
+                modifier = Modifier
+                    .size(350.dp)
+                    .clickable {
+
+                    }
+                    ,
+                composition = composition,
+                progress = { progress }
+            )
 
 
         }
     }
 
 
-}
+
 
 @Preview(showBackground = true)
 @Composable
