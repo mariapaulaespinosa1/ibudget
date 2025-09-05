8f931c322ff7aa758cf5af9e5b43036de0f07cca
estudiante <karolaiturry@gmail.com>
2025-09-05 10:55:31 -0500

    "se agg cajas de objetivos y ahorros"

--- app/src/main/java/com/grupo2/ibudget/screens/MainScreen.kt
+++ app/src/main/java/com/grupo2/ibudget/screens/MainScreen.kt
@@ -3,10 +3,12 @@ package com.grupo2.ibudget.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,15 +19,21 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,7 +54,8 @@ fun MainScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.LightGray)
+            .background(color = Color.LightGray),
+        horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
         Box(
@@ -79,7 +88,16 @@ fun MainScreen() {
 
         }
 
-        Spacer(modifier = Modifier.height((10.dp)))
+        Spacer(modifier = Modifier.height((20.dp)))
+
+        Text(
+            text = "Calendario",
+            color = Color.Black,
+            fontSize = 50.sp, fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
 
         Card(onClick = {
         }, modifier = Modifier.size(300.dp)) {
@@ -88,9 +106,39 @@ fun MainScreen() {
                 contentDescription = null,
                 modifier = Modifier.fillMaxSize()
             )
+
+
         }
 
+        Spacer(modifier = Modifier.height(40.dp))
 
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(40.dp)
+        ) {
+            ElevatedCard(modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .weight(1f)) {
+                Text(text = "OBJTIVOS",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp
+                )
+            }
+
+            ElevatedCard(modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .weight(1f)
+            ) {
+                Text(
+                    text = "AHORROS",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp
+                )
+            }
+        }
     }
 }
 
