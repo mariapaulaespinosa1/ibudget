e6d76adac728b3cf26209d2e028645a85701debc
maria paula <espinosamariapaula98@gmail.com>
2025-11-12 11:06:54 -0500

    Arreglos

--- app/src/main/java/com/grupo2/ibudget/ui/screens/Login.kt
+++ app/src/main/java/com/grupo2/ibudget/ui/screens/Login.kt
@@ -15,9 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text2.BasicSecureTextField
-import androidx.compose.foundation.text2.input.TextFieldState
-import androidx.compose.foundation.text2.input.TextObfuscationMode
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
