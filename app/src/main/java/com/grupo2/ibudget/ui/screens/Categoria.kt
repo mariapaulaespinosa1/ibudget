fb152e49faadf9159d19b1784058b39895e8378a
Maria Espinosa <espinosamariapaula98@gmail.com>
2025-11-16 21:29:53 -0500

    Second Commit

--- app/src/main/java/com/grupo2/ibudget/ui/screens/Categoria.kt
+++ app/src/main/java/com/grupo2/ibudget/ui/screens/Categoria.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,6 +59,6 @@ fun Categoria(imagen: Int, label: String, onClick: () -> Unit) {
 @Composable
 fun CategoriaPreview() {
     IbudgetTheme {
-        Categoria(imagen = R.drawable.cerdito, label = "Ahorros", onClick = {})
+        Categoria(imagen = R.drawable.cerdito, label = stringResource(R.string.categoria_preview_label), onClick = {})
     }
 }
